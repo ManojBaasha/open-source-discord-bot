7980ad0c22e165be25e22d6642503f1fcd974ce9
ManojBaasha <manoj.elango123@gmail.com>
2022-08-11 02:44:07 -0400

    few commands added

--- index.js
+++ index.js
@@ -11,4 +11,34 @@ client.once('ready', () => {
     console.log('Ready!');
 });
 
-client.login(process.env.DISCORD_TOKEN)
+let fs = require("fs")
+const readFileLines = filename =>
+   fs.readFileSync(filename)
+   .toString('UTF8')
+   .split('\n');
+let arr = readFileLines('questions.txt');
+
+
+client.on('interactionCreate',async interaction => {
+    if(!interaction.isChatInputCommand()) return;
+
+    const {commandName} = interaction;
+
+    if(commandName === 'ping'){
+        await interaction.reply('pong');
+    } else if(commandName === 'server'){
+        await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+    } else if(commandName === 'user'){
+        await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+    } else if(commandName === 'question'){
+        await interaction.reply(`${arr[Math.floor(Math.random() * arr.length)]}`);
+    }
+    //more guild commands at https://discord.js.org/#/docs/main/stable/class/Guild
+    //more user commands at https://discord.js.org/#/docs/main/stable/class/User
+});
+
+
+
+
+
+client.login(token)
